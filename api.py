b5d49199dce793cc6afa52685707c06207f920f1
yash-kolhe <83939897+yash-kolhe@users.noreply.github.com>
2021-05-10 12:35:30 +0530

    Making code run faster

--- api.py
+++ api.py
@@ -30,7 +30,8 @@ def retrieveTweet(data_url):
             tweet = json.loads(line)
             tweets_data.append(tweet)
         except:
-            continue            
+            continue 
+
 def retrieveProcessedData(Pdata_url):
     sent = pd.read_excel(Pdata_url)
     for i in range(len(tweets_data)):
@@ -38,53 +39,20 @@ def retrieveProcessedData(Pdata_url):
             x.append(tweets_data[i]['text'])
             y.append(sent['sentiment'][i])
 
-def plot_confusion_matrix(cm, classes,
-                          normalize=False,
-                          title='Confusion matrix',
-                          cmap=plt.cm.Blues):
-    plt.imshow(cm, interpolation='nearest', cmap=cmap)
-    plt.title(title)
-    plt.colorbar()
-    tick_marks = np.arange(len(classes))
-    plt.xticks(tick_marks, classes, rotation=45)
-    plt.yticks(tick_marks, classes)
-
-    fmt = '.2f' if normalize else 'd'
-    thresh = cm.max() / 2.
-    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
-        plt.text(j, i, format(cm[i, j], fmt),
-                 horizontalalignment="center",
-                 color="white" if cm[i, j] > thresh else "black")
-
-    plt.tight_layout()
-    plt.ylabel('True label')
-    plt.xlabel('Predicted label')                     
-
-def datree(string, k):
-    from sklearn import tree
-    start_timedt = time.time()
-    train_featurestree = vectorizer.fit_transform(x)
-    actual1 = y
-    test_features1 = vectorizer.transform(x)
-    dtree = tree.DecisionTreeClassifier()
-    
-    dtree = dtree.fit(train_featurestree, [int(r) for r in y])
-    
-
-def runall():     
-    retrieveTweet('C:/Users/dhuma/OneDrive/Documents/Python/Depression/data/tweetdata.txt')  
-    retrieveProcessedData('C:/Users/dhuma/OneDrive/Documents/Python/Depression/processed_data/output.xlsx')
-    datree('hey',0)
+     
+retrieveTweet('E:\#STUDY\Coding v1\euphoria\data/tweetdata.txt')  
+retrieveProcessedData('E:\#STUDY\Coding v1\euphoria\processed_data/output.xlsx') 
+from sklearn import tree
+train_featurestree = vectorizer.fit_transform(x)
+actual1 = y
+test_features1 = vectorizer.transform(x)
+dtree = tree.DecisionTreeClassifier()
+dtree = dtree.fit(train_featurestree, [int(r) for r in y])
     
 def datreeINPUT(inputtweet):
-    from sklearn import tree
-    train_featurestree = vectorizer.fit_transform(x)
-    dtree = tree.DecisionTreeClassifier()
-    dtree = dtree.fit(train_featurestree, [int(r) for r in y])
     inputdtree= vectorizer.transform([inputtweet])
     predictt = dtree.predict(inputdtree)
     return predictt
-runall()
 
 @app.route('/')
 def home():
@@ -93,14 +61,14 @@ def home():
 
 @app.route('/depression', methods=['GET' , 'POST'])
 def depression():
-    if request.method=='POST':
-        print("post req received")
-        print(request.json)
+    if(request.method=='POST'):
+        print()
+        print("post req received :",request.json)
         answer=datreeINPUT(request.json)
         answer=str(answer[0])
-        print(answer)
+        print("answer ",answer)
         return jsonify(answer)
-    elif request.method=='GET':
+    elif(request.method=='GET'):
         print("get req received")
         print(request)
         return "Hello"
